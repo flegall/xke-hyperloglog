644d832941d04201ff9abedf917f3f49f995e024
Florent Le Gall <florent.legall@gmail.com>
2015-05-29 17:57:27 +0200

    Remove code

--- src/main/scala/hyperloglog/HyperLogLog.scala
+++ src/main/scala/hyperloglog/HyperLogLog.scala
@@ -1,53 +1,32 @@
 package hyperloglog
 
-import java.lang.Long.numberOfLeadingZeros
-import java.lang.Math.{log, max, pow}
+import java.lang.Math.log
 
 import com.google.common.hash.Hashing._
-import hyperloglog.HyperLogLog._
 
 class HyperLogLog(registersBit: Int) {
-  val n = 1 << registersBit
-  val registers = new Array[Int](n)
-  var count = 0
-
-  for (a <- 0 until n) {
-    registers(a) = 0
-  }
+  val n: Int = ???
+  val registers:Array[Int] = ???
+  var count: Int = ???
 
   val biasCorrectionForHyperLogLog = 1.0 / (2.0 * log(2) * (1.0 + (3.0 * log(2) - 1) / n))
   val biasCorrectionForLogLog = 0.395
 
-  def addHash(hashcode: Long): Unit = {
-    val registerIndex = computeRegisterIndex(hashcode, n)
-    val firstOneRank = computeFirstOneRank(hashcode)
-
-    registers(registerIndex) = max(firstOneRank, registers(registerIndex))
-
-    count += 1
-  }
+  def addHash(hashcode: Long): Unit = ???
 
   def addItem(item: Any): Unit = {
     addHash(murmur3_128().hashInt(item.hashCode).asLong)
   }
 
-  def logLogCount: Double =
-    pow(2.0, linearMean(registers)) * n * biasCorrectionForLogLog
+  def logLogCount: Double = ???
 
-  def hyperLogLogCount: Double = {
-    n * n * biasCorrectionForHyperLogLog / registers.map { i =>
-      1.0 / pow(2.0, i)
-    }.sum
-  }
+  def hyperLogLogCount: Double = ???
 }
 
 object HyperLogLog {
-  private[hyperloglog] def linearMean(buckets: Array[Int]): Double =
-    buckets.sum.toDouble / buckets.length.toDouble
+  private[hyperloglog] def linearMean(buckets: Array[Int]): Double = ???
 
-  private[hyperloglog] def computeFirstOneRank(bucketHash: Long): Int =
-    numberOfLeadingZeros(bucketHash) + 1
+  private[hyperloglog] def computeFirstOneRank(bucketHash: Long): Int = ???
 
-  private[hyperloglog] def computeRegisterIndex(hash: Long, n: Int): Int =
-    (hash & (n - 1)).toInt
+  private[hyperloglog] def computeRegisterIndex(hash: Long, n: Int): Int = ???
 }
